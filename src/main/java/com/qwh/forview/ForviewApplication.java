18faed0b296284cc902a799616558dd273df8c8c
qiaoweihao <15061886689@163.com>
2020-04-01 22:25:45 +0800

    第一次提交

---
+++ src/main/java/com/qwh/forview/ForviewApplication.java
@@ -0,0 +1,13 @@
+package com.qwh.forview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qwh/forview/ForviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwh.forview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
